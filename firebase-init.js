14542fd33f120a81687f1158a234c74dc9c7448f
salahram390-sudo <salahram390@gmail.com>
2026-02-20 19:12:41 +0200

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,19 @@
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+// Firebase config بتاعك
+const firebaseConfig = {
+  apiKey: "AIzaSyBY72SkEi6HX4R9fSYAQhnYCLVbPu-W4Ko",
+  authDomain: "meshwarkomm.firebaseapp.com",
+  projectId: "meshwarkomm",
+  storageBucket: "meshwarkomm.firebasestorage.app",
+  messagingSenderId: "889669815551",
+  appId: "1:889669815551:web:b47e9dcf775e4c1eff10ca",
+  measurementId: "G-SDWD0EMRRF"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
